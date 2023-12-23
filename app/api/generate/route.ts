fbae57d82b826402a8a2903c63c52a15acfe2ae5
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2023-12-22 22:04:47 -0500

    Update route.ts
    Trim out unnecessary request headers

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -18,26 +18,13 @@ export async function GET(req: NextRequest) {
     maxBodyLength: Infinity,
     url: 'https://api.discord.gx.games/v1/direct-fulfillment',
     headers: {
-      'authority': 'api.discord.gx.games',
-      'accept': '*/*',
-      'accept-language': 'en-US,en;q=0.9',
-      'content-type': 'application/json',
-      'origin': 'https://www.opera.com',
-      'referer': 'https://www.opera.com/',
-      'sec-ch-ua': '"Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"Windows"',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'cross-site',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0'
+      'Content-Type': 'application/json'
     },
     data: data
   };
 
   try {
-    const res = await axios.request(config);
-    const { token } = res.data;
+    const { token } = (await axios.request(config)).data;
     return new NextResponse(`https://discord.com/billing/partner-promotions/1180231712274387115/${token}`, {
       status: 200,
       headers: {
